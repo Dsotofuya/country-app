a5f9327d96da2f004db05ddda92aecc7c806e635
Daniel Soto Fuya <dsotofuya@gmail.com>
2025-04-06 19:16:06 -0500

    routing for child

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CountriesRoutingModule } from './countries-routing.module';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+import { ByNamePageComponent } from './pages/by-name-page/by-name-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CountryPageComponent,
+    ByNamePageComponent,
+    ByRegionPageComponent,
+    ByCapitalPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    CountriesRoutingModule,
   ]
 })
 export class CountriesModule { }
